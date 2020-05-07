892fb8232a2c664d68dded95ed74fb124a52e885
Muhammad Rizqi <muhrizqi@student.ce.undip.ac.id>
2020-05-07 21:35:59 +0700

    added js calculator

---
+++ WebCalculator/assets/kalkulator.js
@@ -0,0 +1,31 @@
+const calculator ={
+    display: '0',
+    operator:null,
+    firstNumber: null,
+    waitingForSecondNumber: false
+};
+function updateDisplay() {
+    document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecondNumber=false;
+}
+function inputDigit(digit) {
+    calculator.displayNumber += digit;
+}
+const buttons = document.querySelectorAll(".button");
+
+for (let button of buttons) {
+    button.addEventListener('click',function(event)
+    {
+        //mendapatkan object yang diklik
+        const target = event.target;
+        inputDigit(target.innerText);
+        updateDisplay()
+    });
+
+}
